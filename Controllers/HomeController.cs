28bbdd4536a45bee9b8c2d05e2dee24fb8976319
mamunur1296 <mamunurrushid60@gmail.com>
2024-02-13 14:19:10 +0000

    Thim Install success 13 feb 2024

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,13 +6,42 @@ namespace TutorManagementSystem.Controllers
 {
     public class HomeController : Controller
     {
-        
+        private readonly ILogger<HomeController> _logger;
 
-       
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
+        public IActionResult About()
+        {
+            return View();
+        }
+        public IActionResult Services()
+        {
+            return View();
+        }
+        public IActionResult Portfolio()
+        {
+            return View();
+        }
+        public IActionResult Team()
+        {
+            return View();
+        }
+        public IActionResult Blog()
+        {
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
 
         public IActionResult Privacy()
         {
